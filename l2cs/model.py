789e8db646aade7bead2416d851c6c6e5a8e521f
theo <akbas.theo@protonmail.com>
2023-11-30 10:32:13 +0100

    Proofing

--- l2cs/model.py
+++ l2cs/model.py
@@ -9,10 +9,11 @@ class L2CS(nn.Module):
     def __init__(self, block, layers, num_bins):
         self.inplanes = 64
         super(L2CS, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,bias=False)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
